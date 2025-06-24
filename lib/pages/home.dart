d58a397cba7b8d2de137f03c0a66724fe91282ce
MridulSharma2552007 <mridul25sharma52007@gmail.com>
2025-06-24 11:28:23 +0530

    sign out added

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,3 +1,4 @@
+import 'package:akari/authservice/auth_service.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
@@ -5,6 +6,34 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final authService = AuthService();
+    final currentEmail = authService.getUserEmail();
+    void signOut() {
+      authService.signOut();
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+        centerTitle: true,
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 12.0),
+            child: GestureDetector(
+              onTap: () {
+                signOut();
+              },
+              child: Icon(Icons.exit_to_app_rounded),
+            ),
+          ),
+        ],
+      ),
+      body: Center(
+        child: Text(
+          currentEmail ?? 'No user logged in',
+          style: const TextStyle(fontSize: 20),
+        ),
+      ),
+    );
   }
 }
--- lib/pageviewpages/pageone_signin.dart
+++ lib/pageviewpages/pageone_signin.dart
@@ -1,10 +1,159 @@
+import 'package:akari/authservice/auth_service.dart';
+import 'package:akari/pages/home.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-class PageoneSignin extends StatelessWidget {
+class PageoneSignin extends StatefulWidget {
   const PageoneSignin({super.key});
 
   @override
+  State<PageoneSignin> createState() => _PageoneSigninState();
+}
+
+class _PageoneSigninState extends State<PageoneSignin> {
+  final authService = AuthService();
+  final _emailcontroller = TextEditingController();
+  final _passwordcontroller = TextEditingController();
+  final _confirmpasswordcontroller = TextEditingController();
+  //sign up logic
+  Future<void> signUp() async {
+    final email = _emailcontroller.text;
+    final password = _passwordcontroller.text;
+    final confirmpassword = _confirmpasswordcontroller.text;
+    if (email.isEmpty || password.isEmpty || confirmpassword.isEmpty) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text("Please fill in all fields"),
+          backgroundColor: Colors.grey.shade900,
+        ),
+      );
+      return;
+    }
+
+    if (password != confirmpassword) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          duration: Duration(seconds: 2),
+          backgroundColor: Colors.grey.shade900,
+          content: Text("Passwords didn't match"),
+        ),
+      );
+      return;
+    }
+    try {
+      await authService.signUpWithEmailPassword(email, password);
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => Home()),
+      );
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          duration: Duration(seconds: 2),
+          backgroundColor: Colors.grey.shade900,
+          content: Text("Error $e"),
+        ),
+      );
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.blue.shade300,
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(12),
+          child: Container(
+            height: 600,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              color: Colors.white.withValues(alpha: 0.2),
+              borderRadius: BorderRadius.circular(20),
+              border: Border.all(color: Colors.black),
+            ),
+            child: Column(
+              children: [
+                SizedBox(height: 30),
+                Image.asset('assets/images/sign_in.png', height: 50, width: 50),
+                SizedBox(height: 20),
+                Text(
+                  'Sign In',
+                  style: GoogleFonts.lexend(color: Colors.black, fontSize: 30),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(12),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white.withValues(alpha: 0.3),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(12),
+                      child: TextField(
+                        controller: _emailcontroller,
+                        decoration: InputDecoration(labelText: 'Email'),
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(12),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white.withValues(alpha: 0.3),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(12),
+                      child: TextField(
+                        controller: _passwordcontroller,
+                        decoration: InputDecoration(labelText: 'Password'),
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(12),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white.withValues(alpha: 0.3),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(12),
+                      child: TextField(
+                        controller: _confirmpasswordcontroller,
+                        decoration: InputDecoration(
+                          labelText: 'Confirm Password',
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 30),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.black,
+                    padding: EdgeInsets.symmetric(horizontal: 40, vertical: 15),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadiusGeometry.circular(12),
+                    ),
+                  ),
+                  onPressed: () {
+                    signUp();
+                  },
+                  child: Text(
+                    'Get Started',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+                SizedBox(height: 20),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
